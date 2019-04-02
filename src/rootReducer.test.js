08ab27955888e5f1a361497d3646a5a81565a2eb
Gabriela <balicas.gb@gmail.com>
2019-04-02 15:38:49 -0700

    add tests for reducer

---
+++ src/rootReducer.test.js
@@ -0,0 +1,24 @@
+import rootReducer from './rootReducer'
+
+
+describe("rootReducer", () => {
+  it("returns correct state for adding to cart", () => {
+    const result = rootReducer({
+        itemsInCart: []
+    }, {type: "ADD_TO_CART", payload: {item: "test"}})
+
+    expect(result).toEqual({
+        itemsInCart: ["test"]
+    })
+  })
+
+  it("returns correct state for removing from cart", () => {
+    const result = rootReducer({
+        itemsInCart: [{id: 1}]
+    }, {type: "REMOVE_FROM_CART", payload: {id: 1}})
+
+    expect(result).toEqual({
+        itemsInCart: []
+    })
+  })
+})
